dcd236486d47cc1084171b7674b33bf98e1a07ee
Eric <ericrojas1995@gmail.com>
2023-11-29 13:31:50 -0300

    Actualiza quiz_nouns con AppBar

--- lib/screens/quiz_nouns.dart
+++ lib/screens/quiz_nouns.dart
@@ -8,84 +8,91 @@ class QuizNouns extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Material(
-        child: Column(
-          children: [
-            CustomAppBar(
-              title: Text(
-                "Reien",
-                style: Theme.of(context).primaryTextTheme.titleLarge,
-              ),
-            ),
-            ConstrainedBox(
-              constraints: const BoxConstraints(maxHeight: 450),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    width: double.infinity,
-                    margin: const EdgeInsets.all(10),
-                    child: const Text(
-                      'Pregunta 1',
-                      style: TextStyle(fontSize: 28),
-                      textAlign: TextAlign.center,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple[700],
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back),
+            tooltip: "Volver",
+          ),
+          title: const Text("Reien"),
+        ),
+        body: Material(
+          child: Column(
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxHeight: 450),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      margin: const EdgeInsets.all(10),
+                      child: const Text(
+                        'Pregunta 1',
+                        style: TextStyle(fontSize: 28),
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                  ),
-                  Flexible(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: 4,
-                      itemBuilder: (_, index) {
-                        return Container(
-                          margin: const EdgeInsets.all(2.0),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Colors.indigo.shade100, width: 2),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: ListTile(
-                            shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(12),
-                              ),
+                    Flexible(
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: 4,
+                        itemBuilder: (_, index) {
+                          return Container(
+                            margin: const EdgeInsets.all(2.0),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.indigo.shade100, width: 2),
+                              borderRadius: BorderRadius.circular(12),
                             ),
-                            leading: Text('${index + 1}'),
-                            title: const Text('Respuesta'),
-                            onTap: () {},
-                          ),
+                            child: ListTile(
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(12),
+                                ),
+                              ),
+                              leading: Text('${index + 1}'),
+                              title: const Text('Respuesta'),
+                              onTap: () {},
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple[700],
+                      ),
+                      onPressed: () async {
+                        final img =
+                            await rootBundle.load('images/congratulations.png');
+                        final bufferImg = img.buffer;
+                        Share.shareXFiles(
+                          [
+                            XFile.fromData(
+                              bufferImg.asUint8List(
+                                img.offsetInBytes,
+                                img.lengthInBytes,
+                              ),
+                              name: 'Congratulations!',
+                              mimeType: 'image/png',
+                            ),
+                          ],
+                          subject: 'Reien Quiz Results',
                         );
                       },
+                      child: const Text("Share results"),
                     ),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.purple[700],
-                    ),
-                    onPressed: () async {
-                      final img =
-                          await rootBundle.load('images/congratulations.png');
-                      final bufferImg = img.buffer;
-                      Share.shareXFiles(
-                        [
-                          XFile.fromData(
-                            bufferImg.asUint8List(
-                              img.offsetInBytes,
-                              img.lengthInBytes,
-                            ),
-                            name: 'Congratulations!',
-                            mimeType: 'image/png',
-                          ),
-                        ],
-                        subject: 'Reien Quiz Results',
-                      );
-                    },
-                    child: const Text("Share results"),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
